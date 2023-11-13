0392115d819eab87b71b7495dbae351a77c4ef5e
Petukhov Konstantin <kostya49496@gmail.com>
2023-11-13 10:18:42 +0600

    Crash build2

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -12,7 +12,7 @@ class MainKtTest {
         val amount = 6_000
 
         val result = calcCommission(amount, type, prevTransfersInMonth, prevTransfersInDay)
-        assertEquals(0, result)
+        assertEquals(10, result)
     }
 
     @Test
@@ -23,7 +23,7 @@ class MainKtTest {
         val amount = 6_000
 
         val result = calcCommission(amount, type, prevTransfersInMonth, prevTransfersInDay)
-        assertEquals(0, result)
+        assertEquals(20, result)
     }
 
     @Test
